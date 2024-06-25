ba82946437dbb5f8d4ef1e81d8753065bc85b0c9
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-06-25 22:08:59 +0300

    Hello world!

---
+++ lib/app.py
@@ -0,0 +1,13 @@
+# print('Hello World!', end=" ")
+# print("Pass this test, please. ", end="!! ")
+# # print("please." end="!!!\n ")
+# # print("Hello World! Pass this test, please")
+
+
+# Hello World! Pass this test, please.
+
+
+# print('Hello World!' , end=" ")
+# print('Pass this test, please. ', end=" \n ")
+
+print("Hello World! Pass this test, please.", end="")
